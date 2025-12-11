fa66b2b31990a2ccd14a75d4cc8ee3b92f70c836
Koushik <koushiknus@gmail.com>
2025-12-10 21:32:28 -0600

    New : Sealed class example

---
+++ CodeTutor_Compose/app/src/main/java/com/example/composeplayground/kotlin_playground/ApiResult.kt
@@ -0,0 +1,11 @@
+package com.example.composeplayground.kotlin_playground
+
+
+/**
+ * Sealed class Example
+ */
+sealed class ApiResult<out T> {
+    data class Success<T>(val data : T) : ApiResult<T>()
+    data class Error(val message : String, val throwable : Throwable? = null) : ApiResult<Nothing>()
+    object Loading : ApiResult<Nothing>()
+}
